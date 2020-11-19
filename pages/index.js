059ff64dcf301a40e362c920d2922e492f4ef5c6
otsha <otsohaa@gmail.com>
2020-11-19 13:38:24 +0200

    Implement basic category switching

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ProductList from '../components/ProductList'
 import apiService from '../services/apiService'
 
+const categories = [{ name: 'jackets' }, { name: 'shirts' }, { name: 'accessories' }]
+
 const Main = () => {
+  const [category, setCategory] = useState(categories.find(c => c.name === 'jackets'))
   const [products, setProducts] = useState([])
 
-  const handleProductFetch = async () => {
-    const fetchedProducts = await apiService.getCategory('jackets')
-    console.log('fetched', fetchedProducts)
-    fetchedProducts && setProducts(fetchedProducts)
-  }
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const fetchedProducts = await apiService.getCategory(category.name)
+      fetchedProducts && setProducts(fetchedProducts)
+    }
+
+    fetchProducts()
+  }, [category])
 
   return (
     <>
       <h1>Products</h1>
-      <button onClick={handleProductFetch}>Jackets</button>
+      <nav>
+        {categories.map(cat =>
+          <button
+            key={cat.name}
+            onClick={() => setCategory(categories.find(c => c.name === cat.name))}>
+            {cat.name}
+          </button>
+        )}
+      </nav>
       <ProductList products={products} />
     </>
   )
