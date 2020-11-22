3fa0435c5da3d9f3abfd33d315eb93277df93777
otsha <otsohaa@gmail.com>
2020-11-22 13:58:59 +0200

    Usability & styling updates

--- components/ProductList.js
+++ components/ProductList.js
@@ -20,7 +20,7 @@ const ProductList = ({ products }) => {
         {products.map((product) =>
           <tr key={product.id}>
             <td>{product.name}</td>
-            <td>{product.color}</td>
+            <td>{product.color.length > 1 ? product.color.map(col => <span key={col}>{col}, </span>) : product.color[0]}</td>
             <td>{product.manufacturer}</td>
             <td>{product.price}</td>
             <td>{product.availability || 'AVAILABILITY UNKNOWN'}</td>
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from 'react'
 import ProductList from '../components/ProductList'
 import apiService from '../services/apiService'
 
-const categories = [{ name: 'jackets' }, { name: 'shirts' }, { name: 'accessories' }]
+const categories = [{ name: 'Jackets' }, { name: 'Shirts' }, { name: 'Accessories' }]
 
 const Main = () => {
-  const [category, setCategory] = useState(categories.find(c => c.name === 'jackets'))
+  const [category, setCategory] = useState(categories.find(c => c.name === 'Jackets'))
   const [products, setProducts] = useState([])
   const [error, setError] = useState('')
 
   useEffect(() => {
+    setProducts(null)
     const fetchProducts = async () => {
       const fetchedProducts = await apiService.getProductsByCategory(category.name)
       fetchedProducts.error
@@ -22,18 +23,19 @@ const Main = () => {
 
   return (
     <>
-      <h1>Products</h1>
+      <h1>Product Catalog</h1>
       <nav>
         {categories.map(cat =>
           <button
+            className={cat.name === category.name ? 'active-category' : ''}
             key={cat.name}
             onClick={() => setCategory(categories.find(c => c.name === cat.name))}>
             {cat.name}
           </button>
         )}
-      </nav>
-      {error && <p>{error}</p>}
-      <ProductList products={products} />
+    </nav>
+      { error && <p>{error}</p> }
+  <ProductList products={products} />
     </>
   )
 }
